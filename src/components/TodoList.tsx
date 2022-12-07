578d28250c3eb79f5378ee03507836d5460e74d5
Olga Dementey <dementeyolya@gmail.com>
2022-12-07 08:10:49 +0300

    refactor: change folder structure and add TodoList  component template

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface TodoProps {
+
+}
+
+const TodoList: React.FC<TodoProps> = () => {
+    return (
+        <div>
+            todo list
+        </div>
+    );
+};
+
+export default TodoList;
